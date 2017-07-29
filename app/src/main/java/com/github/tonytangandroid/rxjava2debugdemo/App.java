1272b128f56d7ce38d14eb2acadf8787d55f3e32
ztang <ztang@microstrategy.com>
2017-07-29 10:57:58 -0400

    add comment

--- app/src/main/java/com/github/tonytangandroid/rxjava2debugdemo/App.java
+++ app/src/main/java/com/github/tonytangandroid/rxjava2debugdemo/App.java
@@ -1,13 +1,17 @@
 package com.github.tonytangandroid.rxjava2debugdemo;
 
 import android.app.Application;
+import android.util.Log;
+
+import com.akaita.java.rxjava2debug.RxJava2Debug;
 
 public class App extends Application {
 
     @Override
     public void onCreate() {
         super.onCreate();
-//        RxJava2Debug.enableRxJava2AssemblyTracking();
 
+        RxJava2Debug.enableRxJava2AssemblyTracking();
+        Log.i("App", "RxJava2Debug enableRxJava2AssemblyTracking ");
     }
 }
